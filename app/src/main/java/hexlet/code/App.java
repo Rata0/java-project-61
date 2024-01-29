6de75cbdededb433fae492162ee6e894c945f4b1
Alexander <nekon689@gmail.com>
2024-01-29 22:04:08 +0300

    modification App and Cli files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
+import hexlet.code.games.EvenGame;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.runCli();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int input = scanner.nextInt();
+
+        switch (input) {
+            case 2:
+                EvenGame.startGame();
+                break;
+            case 1:
+                Cli.runCli();
+                break;
+            case 0:
+                System.out.println("Exiting the program.");
+                break;
+            default:
+                System.out.println("Invalid choice. Exiting the program. Goodbye!");
+        }
     }
 }
