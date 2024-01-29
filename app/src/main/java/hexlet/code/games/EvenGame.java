88ebbde90f76cc9464445f20ccbcf65418b0b47b
Alexander <nekon689@gmail.com>
2024-01-29 22:03:42 +0300

    add file EvenGame

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,2 +1,39 @@
 package hexlet.code.games;
 
+import java.util.Scanner;
+import hexlet.code.Cli;
+
+public class EvenGame {
+    public static void startGame() {
+        Cli.runCli();
+        String name = Cli.userName;
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        for (int correctAnswers = 0; correctAnswers < 3; correctAnswers += 1) {
+            int number = generateRandomNumber();
+            System.out.println("Question: " + number);
+            System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.nextLine();
+
+            if ((isEven(number) ? "yes" : "no").equals(answer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. "
+                        + "Correct answer was '" + (isEven(number) ? "yes" : "no") + "'.");
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+
+        System.out.println("Congratulations, " + name + "!");
+    }
+
+    static int generateRandomNumber() {
+        return (int) (Math.random() * 100) + 1;
+    }
+
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
