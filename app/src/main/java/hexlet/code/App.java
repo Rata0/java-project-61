e36b32e15d9f332c3f5684ebc0f26a0f92ce1e8b
Alexander <nekon689@gmail.com>
2024-01-29 11:23:46 +0300

    update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,9 @@
 package hexlet.code;
+
+import java.util.Scanner;
+import hexlet.code.Cli;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Cli.runCli();
     }
 }
