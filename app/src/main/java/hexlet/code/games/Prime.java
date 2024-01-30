161dca691f9b15df1736757f97ed42917ca10e6a
Alexander <nekon689@gmail.com>
2024-01-30 15:27:16 +0300

    add game Prime number

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,34 @@
+package hexlet.code.games;
+
+import static hexlet.code.Engine.gameLauncher;
+import static hexlet.code.Engine.generateRandomNumber;
+public class Prime {
+    public static void startGame() {
+        String gameTitle = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[][] questionAndAnswer = new String[3][2];
+
+        for (int index = 0; index < 3; index += 1) {
+            int number = generateRandomNumber();
+            questionAndAnswer[index][0] = Integer.toString(number);
+
+            String response = isPrime(number) ? "yes" : "no";
+            questionAndAnswer[index][1] = response;
+        }
+
+        gameLauncher(gameTitle, questionAndAnswer);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i += 1) {
+            if (number % 2 == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
