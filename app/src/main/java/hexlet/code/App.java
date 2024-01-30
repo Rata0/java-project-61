bbc8bff503a44d15dc150ad788b442362e33d1a2
Alexander <nekon689@gmail.com>
2024-01-30 13:26:25 +0300

    correction

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.games.EvenGame;
+import hexlet.code.games.CalculatorGame;
 
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int input = scanner.nextInt();
@@ -23,6 +25,9 @@ public class App {
                 break;
             case 2:
                 EvenGame.startGame();
+                break;
+            case 3:
+                CalculatorGame.startGame();
                 break;
             default:
                 System.out.println("Invalid choice. Exiting the program. Goodbye!");
