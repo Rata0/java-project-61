184dcfd32f734f005e04c7aea646a0404640e313
Alexander <nekon689@gmail.com>
2024-01-30 20:24:42 +0300

    fix code Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -22,17 +22,17 @@ public class Prime {
         gameLauncher(gameTitle, questionAndAnswer);
     }
 
+
     public static boolean isPrime(int number) {
         if (number <= 1) {
             return false;
         }
-
-        for (int i = 2; i <= Math.sqrt(number); i += 1) {
-            if (number % 2 == 0) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
-
         return true;
     }
+
 }
